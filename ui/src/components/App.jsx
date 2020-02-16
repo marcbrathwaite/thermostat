137b40be7242217b860463fce00fac930b99736b
Marc Brathwaite <marcbrathwaite@gmail.com>
2020-02-16 06:55:09 -0500

    feat: 🎸 start material ui styling

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 const App = ({ children }) => {
   // TODO: CSS reset
 
-  return <>{children}</>
+  return (
+    <>
+      <CssBaseline />
+      {children}
+    </>
+  )
 }
 
 // TODO: proptypes
--- ui/src/components/ControlPanel/ThermostatStatus/index.jsx
+++ ui/src/components/ControlPanel/ThermostatStatus/index.jsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 // Manager
 import ThermostatManager from '../../../managers/ThermostatManager'
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  testBorder: {
+    border: '1px solid black'
+  }
+}))
+
 const Status = () => {
+  const classes = useStyles()
   const thermostatManager = new ThermostatManager()
   const [currentTemp, setCurrentTemp] = useState({
     status: 'UNINIT',
@@ -15,7 +27,7 @@ const Status = () => {
     const asynFn = async () => {
       try {
         setCurrentTemp({ status: 'LOADING' })
-        const data = await thermostatManager.getCurrentTemp('hshgdsg$%')
+        const data = await thermostatManager.getCurrentTemp('Toronto')
         console.log(data)
         setCurrentTemp({
           status: 'SUCCESS',
@@ -30,13 +42,47 @@ const Status = () => {
     }
   }, [thermostatManager])
 
-  if (isLoading) {
-    return <div>Loading</div>
-  }
-  if (isError) {
-    return <div>Error</div>
-  }
-  return <div>Current Temp: {currentTemp.value}</div>
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3} className={classes.testBorder}>
+        <Grid
+          container
+          item
+          justify="center"
+          xs={12}
+          className={classes.testBorder}
+        >
+          <Grid
+            item
+            container
+            xs={3}
+            className={classes.testBorder}
+            justify="center"
+          >
+            Status: ON
+          </Grid>
+          <Grid
+            item
+            container
+            xs={3}
+            className={classes.testBorder}
+            justify="center"
+          >
+            Fsn: Auto
+          </Grid>
+        </Grid>
+        <Grid item xs={4} className={classes.testBorder}>
+          Outside Temp
+        </Grid>
+        <Grid item xs={4} className={classes.testBorder}>
+          SetPoint Temp
+        </Grid>
+        <Grid item xs={4} className={classes.testBorder}>
+          Indoor Temp
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default Status

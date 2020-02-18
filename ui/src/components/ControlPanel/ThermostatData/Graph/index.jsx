504bfc962def1681ae20b686c9ab6d629c770569
Marc Brathwaite <marcbrathwaite@gmail.com>
2020-02-18 07:29:18 -0500

    feat: 🎸 display graph

--- ui/src/components/ControlPanel/ThermostatData/Graph/index.jsx
+++ ui/src/components/ControlPanel/ThermostatData/Graph/index.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from 'recharts'
 
-const Graph = () => {
-  return <div>Graph</div>
+const Graph = ({ data, dateRange }) => {
+  console.log('data', data)
+  // TODO: move to manager
+  const points = []
+  const { graph_data } = data[0]
+  for (let i = 0; i < graph_data.length; i++) {
+    const point = {
+      x: graph_data[i].x,
+      [data[0].id]: graph_data[i].actual
+    }
+    for (let j = 1; j < data.length; j++) {
+      const { graph_data } = data[j]
+      point[data[j].id] = graph_data[i].actual
+    }
+    points.push(point)
+  }
+  console.log(points)
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <LineChart data={points}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="x" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {data.map(d => {
+          return (
+            <Line key={d.id} type="monotone" dataKey={d.id} stroke="#8884d8" />
+          )
+        })}
+      </LineChart>
+    </ResponsiveContainer>
+  )
 }
 
 export default Graph
